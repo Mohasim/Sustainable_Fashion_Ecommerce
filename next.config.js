2b6e80fe4d9a805dd7db51f11b47c8ebad725562
Mohasim <Mmansuri8381@conestogac.on.ca>
2023-12-25 17:25:19 -0500

    config fix2

--- next.config.js
+++ next.config.js
@@ -3,11 +3,13 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/@/app/api/(.*)",
+        source: "/api/:path*",
         headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          // Add other headers as needed
-        ],
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
       },
     ];
   },
